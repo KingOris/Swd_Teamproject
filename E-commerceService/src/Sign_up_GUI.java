9ca910c312abdb03838b9ecda1ba28b13b4df7f9
Kaiqiang Zhang <kaiqzhang@uiowa.edu>
2018-11-29 20:14:35 -0600

    sing up GUI complete

---
+++ E-commerceService/src/Sign_up_GUI.java
@@ -0,0 +1,90 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Sign_up_GUI extends JFrame implements ActionListener {
+    private JTextField IDField = new JTextField(10);
+    private JTextField pWField = new JTextField(10);
+    private JTextField eField = new JTextField(10);
+    private JTextField pNField = new JTextField(10);
+    private JComboBox bar = generateBar();
+    private int buyerOrSeller = 0;
+    private JButton signUp = new JButton("Sign Up");
+    public Sign_up_GUI(){
+        super("Register");
+        initialSignUp();
+        signUp.addActionListener(this);
+        bar.addActionListener(this);
+
+        this.setVisible(true);
+        this.setSize(400,200);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void initialSignUp(){
+
+        JPanel panel = new JPanel(new GridLayout(5,2));
+        JLabel ID = new JLabel("ID: ");
+        ID.setHorizontalAlignment(JLabel.RIGHT);
+        JLabel password = new JLabel("Password");
+        password.setHorizontalAlignment(JLabel.RIGHT);
+        JLabel email = new JLabel("Email: ");
+        email.setHorizontalAlignment(JLabel.RIGHT);
+        JLabel phoneNum = new JLabel("Phone Number: ");
+        phoneNum.setHorizontalAlignment(JLabel.RIGHT);
+
+
+
+        panel.add(ID);
+        panel.add(IDField);
+        panel.add(password);
+        panel.add(pWField);
+        panel.add(email);
+        panel.add(eField);
+        panel.add(phoneNum);
+        panel.add(pNField);
+        panel.add(bar);
+        panel.add(signUp);
+
+        this.add(panel);
+    }
+
+    private JComboBox<String> generateBar(){
+        String[] choice = {"Buyer", "Seller", "Seller and Buyer"};
+        JComboBox<String> bar = new JComboBox<>(choice);
+        bar.setSelectedIndex(1);
+
+        return bar;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == bar){
+            JComboBox cb = (JComboBox)e.getSource();
+            String msg = (String)cb.getSelectedItem();
+            if (msg != null) {
+                switch(msg){
+                    case "Buyer" :
+                        buyerOrSeller = 1;
+                    break;
+                    case "Seller" :
+                        buyerOrSeller = 2;
+                    break;
+                    case "Seller and Buyer" :
+                        buyerOrSeller = 3;
+                }
+            }
+        }else if(e.getSource() == signUp){
+            signUp.setText("Clicked");
+        }
+    }
+
+
+    public static void main(String args[]){
+        new Sign_up_GUI();
+
+    }
+}
+
+
