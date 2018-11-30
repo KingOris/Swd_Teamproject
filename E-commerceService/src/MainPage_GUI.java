f1d1571705bd88057a499a4e62a2984cb13010ac
Kaiqiang Zhang <kaiqzhang@uiowa.edu>
2018-11-30 01:47:51 -0600

    add main page

--- E-commerceService/src/MainPage_GUI.java
+++ E-commerceService/src/MainPage_GUI.java
@@ -3,29 +3,56 @@ import java.awt.*;
 
 public class MainPage_GUI extends JFrame{
     private JButton logButton = new JButton("Log In");
-    private int goodsNumber = 10;
+    private int goodsNumber = 100;
     private JPanel mainPanel;
-    public MainPage_GUI(){
+
+    // crate products detail panel
+    private JLabel[] goodsName = new JLabel[goodsNumber];
+    private JLabel[] goodsPrice = new JLabel[goodsNumber];
+    private JLabel[] goodsQuality = new JLabel[goodsNumber];
+    private JButton[] goodsBuy =  new JButton[goodsNumber];
+    private JPanel[] panel = new JPanel[goodsNumber];
+
+    private JTextField searchBar = new JTextField(20);
+    private JButton search = new JButton("Search");
+
+    private MainPage_GUI(){
         super("Main Page");
-        mainPanel = new JPanel(new GridLayout(goodsNumber,1));
+        mainPanel = new JPanel(new GridLayout(goodsNumber+2,1));
         initialMainPage(goodsNumber);
-        JScrollPane scrol = new JScrollPane(mainPanel,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        this.add(scrol);
+        JScrollPane scroll = new JScrollPane(mainPanel,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        this.add(scroll);
         this.setVisible(true);
-        this.setSize(400,200);
+        this.setSize(600,500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     private void initialMainPage(int n){
-        JLabel[] goodsName = new JLabel[n];
-        JLabel[] goodsPrice = new JLabel[n];
-        JLabel[] goodsQuality = new JLabel[n];
-        JButton[] goodsBuy =  new JButton[n];
-        JPanel[] panel = new JPanel[n];
+
+
+        //crate search bar
+        JPanel searchPanel = new JPanel(new FlowLayout());
+        searchPanel.add(searchBar);
+        searchPanel.add(search);
+        mainPanel.add(searchPanel);
+
+
+        //set title for products
+        JLabel Name = new JLabel("Products Name");
+        JLabel price = new JLabel("Price");
+        JLabel amount = new JLabel("Amount");
+        JLabel block = new JLabel();
+        JPanel title = new JPanel(new GridLayout(1,4));
+        title.add(Name);
+        title.add(price);
+        title.add(amount);
+        title.add(block);
+        mainPanel.add(title);
         for(int i =0; i< n; i++){
             goodsName[i] = new JLabel("This is " + i + "'st " + "product");
             goodsPrice[i]= new JLabel("The price is " + i);
             goodsQuality[i] = new JLabel("The quality is" + i);
             goodsBuy[i] = new JButton("Buy");
+            //goodsBuy[i].setPreferredSize(new Dimension(3,2));
 
 
             panel[i] = new JPanel();
@@ -38,7 +65,11 @@ public class MainPage_GUI extends JFrame{
             mainPanel.add(panel[i]);
         }
         this.add(mainPanel);
+
+
+        this.pack();
     }
+
 
     public static void main(String[] args){
         new MainPage_GUI();
