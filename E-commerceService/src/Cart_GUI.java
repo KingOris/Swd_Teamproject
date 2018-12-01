431e60ff883f1273f410daf6e7c2cc988f7afadf
ychen270 <yuhang-chen@uiowa.edu>
2018-11-30 21:41:20 -0600

    Add mark all and un-mark all action listener

--- E-commerceService/src/Cart_GUI.java
+++ E-commerceService/src/Cart_GUI.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.math.BigDecimal;
@@ -10,6 +12,8 @@ import javax.swing.JCheckBox;
 public class Cart_GUI extends JFrame {
     private JPanel mainPanel;
     private int goodsNumber;
+    private JButton markAll;
+    private JButton remove;
     private JCheckBox[] goodsBuy;
     private JLabel[] goodsName;
     private JLabel[] goodsQuality;
@@ -17,19 +21,26 @@ public class Cart_GUI extends JFrame {
     private JLabel[] goodsPrice;
     private JLabel total;
     private JLabel totalAmount;
+    private boolean check;
+    private BigDecimal totalPrice;
 
     //public Cart_GUI(int goodsNumber, ArrayList<String> name, ArrayList<String> price, ArrayList<Integer> amout) {
     public Cart_GUI(int goodsNumber) {
         super("Cart");
         this.goodsNumber = goodsNumber;
         mainPanel = new JPanel(new GridLayout(this.goodsNumber + 2, 1, 1, 6));
-        panel = new JPanel[this.goodsNumber ];
+        panel = new JPanel[this.goodsNumber];
         goodsBuy = new JCheckBox[this.goodsNumber];
         goodsName = new JLabel[this.goodsNumber];
         goodsQuality = new JLabel[this.goodsNumber];
         goodsPrice = new JLabel[this.goodsNumber];
         total = new JLabel("Total : ");
         totalAmount = new JLabel();
+        remove = new JButton("Remove");
+        markAll = new JButton("Mark/Un-mark All");
+        check = true;
+        totalPrice = new BigDecimal("0");
+
 
         JLabel Name = new JLabel("Products Name");
         Name.setHorizontalAlignment(JLabel.LEFT);
@@ -39,28 +50,31 @@ public class Cart_GUI extends JFrame {
         amount.setHorizontalAlignment(JLabel.LEFT);
         JLabel mark = new JLabel("Mark");
         amount.setHorizontalAlignment(JLabel.LEFT);
-        JPanel title = new JPanel(new GridLayout(1,4));
+        JPanel title = new JPanel(new GridLayout(1, 4));
         title.add(Name);
         title.add(price);
         title.add(amount);
         title.add(mark);
         mainPanel.add(title);
 
-        for(int i = 0; i<this.goodsNumber; i++){
+        CheckAll action = new CheckAll();
+        markAll.addActionListener(action);
+        for (int i = 0; i < this.goodsNumber; i++) {
             //setItempanel(name.get(i),price.get(i),amout.get(i),i);
-            setItempanel("name","100",10,i);
+            setItempanel("name", "100", 10, i);
         }
-        
+
+        totalAmount.setText(totalPrice.toString());
 
         JPanel totalPanel = new JPanel();
         totalPanel.add(total);
         totalPanel.add(totalAmount);
-
+        totalPanel.add(markAll);
 
         mainPanel.add(totalPanel);
 
         add(mainPanel);
-        JScrollPane scroll = new JScrollPane(mainPanel,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        JScrollPane scroll = new JScrollPane(mainPanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         add(scroll);
 
 
@@ -68,14 +82,19 @@ public class Cart_GUI extends JFrame {
 
 
     public void setItempanel(String itemname, String itemprice, int amount, int i) {
-        goodsName[i] = new JLabel( itemname);
-        goodsPrice[i]= new JLabel( itemprice);
-        goodsQuality[i]= new JLabel("" + amount);
-        goodsBuy[i] = new JCheckBox("",true);
+        goodsName[i] = new JLabel(itemname);
+        goodsPrice[i] = new JLabel(itemprice);
+        goodsQuality[i] = new JLabel("" + amount);
+        goodsBuy[i] = new JCheckBox("", true);
         CheckBoxHandler handler = new CheckBoxHandler();
         goodsBuy[i].addItemListener(handler);
 
-        panel[i] = new JPanel(new GridLayout(1,4));
+        panel[i] = new JPanel(new GridLayout(1, 4));
+
+        if (goodsBuy[i].isSelected()) {
+            BigDecimal ppp = new BigDecimal(goodsPrice[i].getText());
+            totalPrice = totalPrice.add(ppp);
+        }
 
         panel[i].add(goodsName[i]);
         panel[i].add(goodsPrice[i]);
@@ -87,23 +106,42 @@ public class Cart_GUI extends JFrame {
     private class CheckBoxHandler implements ItemListener {
         @Override
         public void itemStateChanged(ItemEvent e) {
-            BigDecimal totalPrice = new BigDecimal("0");
+            check = true;
+            totalPrice = BigDecimal.ZERO;
             for (int i = 0; i < goodsNumber; i++) {
                 if (goodsBuy[i].isSelected()) {
                     BigDecimal ppp = new BigDecimal(goodsPrice[i].getText());
                     totalPrice = totalPrice.add(ppp);
-       totalAmount.setText(totalPrice.toString());
-   }
-}
-        }
+                } else {
+                    check = false;
                 }
+            }
+            totalAmount.setText(totalPrice.toString());
+        }
+    }
 
-public static void main(String[] args){
+    private class CheckAll implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            if (check) {
+                for (int i = 0; i < goodsNumber; i++) {
+                    goodsBuy[i].setSelected(false);
+                }
+            } else {
+                for (int i = 0; i < goodsNumber; i++) {
+                    goodsBuy[i].setSelected(true);
+                }
+            }
+        }
+    }
+/*public static void main(String[] args){
         Cart_GUI cart_gui = new Cart_GUI(50);
         cart_gui.setSize(600,400);
         cart_gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         cart_gui.setResizable(true);
         cart_gui.setVisible(true);
-    }
+    }*/
 }
 
