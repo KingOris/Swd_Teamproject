93a856248c33c80201e3bbd16c3f752e59a2b232
ychen270 <yuhang-chen@uiowa.edu>
2018-11-30 23:20:30 -0600

    add remove button

--- E-commerceService/src/Cart_GUI.java
+++ E-commerceService/src/Cart_GUI.java
@@ -23,6 +23,8 @@ public class Cart_GUI extends JFrame {
     private JLabel totalAmount;
     private boolean check;
     private BigDecimal totalPrice;
+    private ArrayList<Integer> removeNum;
+    private JPanel totalPanel;
 
     //public Cart_GUI(int goodsNumber, ArrayList<String> name, ArrayList<String> price, ArrayList<Integer> amout) {
     public Cart_GUI(int goodsNumber) {
@@ -40,8 +42,37 @@ public class Cart_GUI extends JFrame {
         markAll = new JButton("Mark/Un-mark All");
         check = true;
         totalPrice = new BigDecimal("0");
+        removeNum = new ArrayList<>();
 
 
+        setTitle();
+
+        CheckAll action = new CheckAll();
+        markAll.addActionListener(action);
+
+        RemoveHandler removeHandler = new RemoveHandler();
+        remove.addActionListener(removeHandler);
+
+        for (int i = 0; i < this.goodsNumber; i++) {
+            //setItempanel(name.get(i),price.get(i),amout.get(i),i);
+            setItempanel("name", "100", 10, i);
+        }
+
+
+
+        totalAmount.setText(totalPrice.toString());
+
+        totalPanel = new JPanel();
+        setTotalPanel();
+
+        add(mainPanel);
+        JScrollPane scroll = new JScrollPane(mainPanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        add(scroll);
+
+
+    }
+
+    private void setTitle(){
         JLabel Name = new JLabel("Products Name");
         Name.setHorizontalAlignment(JLabel.LEFT);
         JLabel price = new JLabel("Price");
@@ -56,31 +87,7 @@ public class Cart_GUI extends JFrame {
         title.add(amount);
         title.add(mark);
         mainPanel.add(title);
-
-        CheckAll action = new CheckAll();
-        markAll.addActionListener(action);
-        for (int i = 0; i < this.goodsNumber; i++) {
-            //setItempanel(name.get(i),price.get(i),amout.get(i),i);
-            setItempanel("name", "100", 10, i);
-        }
-
-
-        totalAmount.setText(totalPrice.toString());
-
-        JPanel totalPanel = new JPanel();
-        totalPanel.add(total);
-        totalPanel.add(totalAmount);
-        totalPanel.add(markAll);
-
-        mainPanel.add(totalPanel);
-
-        add(mainPanel);
-        JScrollPane scroll = new JScrollPane(mainPanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        add(scroll);
-
-
     }
-
 
     public void setItempanel(String itemname, String itemprice, int amount, int i) {
         goodsName[i] = new JLabel(itemname);
@@ -136,6 +143,31 @@ public class Cart_GUI extends JFrame {
                 }
             }
         }
+    }
+
+    private class RemoveHandler implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            for (int i = 0; i < goodsNumber; i++) {
+                if (!goodsBuy[i].isSelected()) {
+                    removeNum.add(i);
+                }
+        }
+    }
+    }
+
+    public void setTotalPanel(){
+        totalAmount.setText(totalPrice.toString());
+        totalPanel.add(total);
+        totalPanel.add(totalAmount);
+        totalPanel.add(markAll);
+        totalPanel.add(remove);
+        mainPanel.add(totalPanel);
+    }
+
+    public ArrayList<Integer> getRemoveNum(){
+        return removeNum;
     }
 /*public static void main(String[] args){
         Cart_GUI cart_gui = new Cart_GUI(50);
